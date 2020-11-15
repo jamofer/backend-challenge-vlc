1c34179388d987763a136d5cc437f7a496a03f35
Javier Moragon <jamofer@gmail.com>
2020-11-15 19:49:14 +0100

    Use pytest syntax for tests

--- python/test.py
+++ python/test.py
@@ -10,13 +10,11 @@ class TestOrder(TestCase):
         order.add_product(Product(name='Product 2', type=ProductType.BOOK, price=11.0), 1)
         order.add_product(Product(name='Product 3', type=ProductType.PHYSICAL, price=12.0), 1)
 
-        self.assertEqual(len(order.items), 3)
+        assert len(order.items) == 3
 
     def test_total_amount(self):
         order = Order(Customer())
         order.add_product(Product(name='Product 1', type=ProductType.DIGITAL, price=10), quantity=2)
         order.add_product(Product(name='Product 2', type=ProductType.DIGITAL, price=15), quantity=2)
 
-        total = order.total_amount()
-
-        self.assertEqual(total, 50)
+        assert order.total_amount() == 50
